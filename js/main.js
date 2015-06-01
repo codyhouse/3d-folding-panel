94fe62657bab7869d8af9cdc675266944cba3a61
claudia-romano <claudia.romano06@gmail.com>
2015-06-01 12:54:49 +0100

    fixed bug - content below the gallery
    scroll-up when additional content is visible below the gallery

--- js/main.js
+++ js/main.js
@@ -1,30 +1,39 @@
 jQuery(document).ready(function($){
+	var gallery = $('.cd-gallery'),
+		foldingPanel = $('.cd-folding-panel'),
+		mainContent = $('.cd-main');
 	/* open folding content */
-	$('.cd-gallery a').on('click', function(event){
+	gallery.on('click', 'a', function(event){
 		event.preventDefault();
 		openItemInfo($(this).attr('href'));
 	});
 
 	/* close folding content */
-	$('.cd-folding-panel .cd-close').on('click', function(event){
+	foldingPanel.on('click', '.cd-close', function(event){
 		event.preventDefault();
 		toggleContent('', false);
 	});
-	$('.cd-gallery').on('click', function(event){
+	gallery.on('click', function(event){
 		/* detect click on .cd-gallery::before when the .cd-folding-panel is open */
 		if($(event.target).is('.cd-gallery') && $('.fold-is-open').length > 0 ) toggleContent('', false);
 	})
 
 	function openItemInfo(url) {
 		var mq = viewportSize();
-		if( $('.cd-gallery').offset().top > $(window).scrollTop() && mq != 'mobile') {
+		if( gallery.offset().top > $(window).scrollTop() && mq != 'mobile') {
 			/* if content is visible above the .cd-gallery - scroll before opening the folding panel */
 			$('body,html').animate({
-				'scrollTop': $('.cd-gallery').offset().top
+				'scrollTop': gallery.offset().top
 			}, 100, function(){ 
 	           	toggleContent(url, true);
 	        }); 
-
+	    } else if( gallery.offset().top + gallery.height() < $(window).scrollTop() + $(window).height()  && mq != 'mobile' ) {
+			/* if content is visible below the .cd-gallery - scroll before opening the folding panel */
+			$('body,html').animate({
+				'scrollTop': gallery.offset().top + gallery.height() - $(window).height()
+			}, 100, function(){ 
+	           	toggleContent(url, true);
+	        });
 		} else {
 			toggleContent(url, true);
 		}
@@ -33,28 +42,28 @@ jQuery(document).ready(function($){
 	function toggleContent(url, bool) {
 		if( bool ) {
 			/* load and show new content */
-			var foldingContent = $('.cd-fold-content');
+			var foldingContent = foldingPanel.find('.cd-fold-content');
 			foldingContent.load(url+' .cd-fold-content > *', function(event){
 				setTimeout(function(){
 					$('body').addClass('overflow-hidden');
-					$('.cd-folding-panel').addClass('is-open');
-					$('.cd-main').addClass('fold-is-open');
+					foldingPanel.addClass('is-open');
+					mainContent.addClass('fold-is-open');
 				}, 100);
 				
 			});
 		} else {
 			/* close the folding panel */
 			var mq = viewportSize();
-			$('.cd-folding-panel').removeClass('is-open');
-			$('.cd-main').removeClass('fold-is-open');
+			foldingPanel.removeClass('is-open');
+			mainContent.removeClass('fold-is-open');
 			
 			(mq == 'mobile' || $('.no-csstransitions').length > 0 ) 
 				/* according to the mq, immediately remove the .overflow-hidden or wait for the end of the animation */
 				? $('body').removeClass('overflow-hidden')
 				
-				: $('.cd-main').find('.cd-item').eq(0).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				: mainContent.find('.cd-item').eq(0).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
 					$('body').removeClass('overflow-hidden');
-					$('.cd-main').find('.cd-item').eq(0).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+					mainContent.find('.cd-item').eq(0).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
 				});
 		}
 		
